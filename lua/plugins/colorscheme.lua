16c5dd65642c5f607ed81501b0c8e73000c7c435
throwea <saeruig@saeruig.localdomain>
2025-09-14 01:27:46 -0500

    reverted colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      -- overrides = {
+      --   ["@lsp.type.function"] = { bg = "#ff9900", bold = true },
+      --   ["@comment.lua"] = { bg = "#000000", bold = false, italic = false },
+      -- },
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
