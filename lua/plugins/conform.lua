8b84c0a7023f96379bc48b9e54af1d1e499b5b6d
throwea <saeruig@saeruig.localdomain>
2025-10-01 08:48:46 -0500

    saving conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,30 +17,30 @@ return { -- Autoformat
       pattern = "go",
       callback = function()
         vim.opt_local.expandtab = false -- use real tabs
-        vim.opt_local.tabstop = 4       -- visual width of a tab
-        vim.opt_local.shiftwidth = 4    -- >> and autoindent = 1 tab
+        vim.opt_local.tabstop = 4 -- visual width of a tab
+        vim.opt_local.shiftwidth = 4 -- >> and autoindent = 1 tab
         vim.opt_local.softtabstop = 4
       end,
     })
   end,
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = "never"
-      else
-        lsp_format_opt = "fallback"
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
+    -- format_on_save = function(bufnr)
+    --   -- Disable "format_on_save lsp_fallback" for languages that don't
+    --   -- have a well standardized coding style. You can add additional
+    --   -- languages here or re-enable it for the disabled ones.
+    --   local disable_filetypes = { c = true, cpp = true }
+    --   local lsp_format_opt
+    --   if disable_filetypes[vim.bo[bufnr].filetype] then
+    --     lsp_format_opt = "never"
+    --   else
+    --     lsp_format_opt = "fallback"
+    --   end
+    --   return {
+    --     timeout_ms = 500,
+    --     lsp_format = lsp_format_opt,
+    --   }
+    -- end,
     formatters_by_ft = {
       lua = { "stylua" },
       go = { "goimports", "gofumpt", "gotmpl" },
