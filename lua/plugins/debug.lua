e8a4064aa85b66f6aeedd5c072c6bd69bf6a1a27
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2025-10-02 18:36:06 -0500

    remobed import

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -5,7 +5,7 @@
 -- Primarily focused on configuring the debugger for Go, but can
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
-local utils = require("utils")
+-- local utils = require("utils")
 
 return {
   -- NOTE: Yes, you can install new plugins here!
