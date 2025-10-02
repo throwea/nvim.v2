aeb46c38e79e65bb52d6af208845588843fda776
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2025-10-02 18:33:52 -0500

    corrected import

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -29,6 +29,7 @@ return {
   keys = function(_, keys)
     local dap = require("dap")
     local dapui = require("dapui")
+    local snacks = require("snacks")
     return {
       -- Basic debugging keymaps, feel free to change to your liking!
       { "<F5>", dap.continue, desc = "Debug: Start/Continue" },
@@ -36,7 +37,7 @@ return {
       { "<F2>", dap.step_over, desc = "Debug: Step Over" },
       { "<F3>", dap.step_out, desc = "Debug: Step Out" },
       { "<F4>", dap.close, desc = "Debug: Stop Process" },
-      { "<F6>", Snacks.picker(dap.list_breakpoints(false)), desc = "Debug: Stop Process" },
+      { "<F6>", snacks.picker(dap.list_breakpoints(false)), desc = "Debug: Stop Process" },
       { "<F8>", dap.restart_frame, desc = "Debug: Stop Process" },
       { "<leader>b", dap.toggle_breakpoint, desc = "Debug: Toggle Breakpoint" },
       {
