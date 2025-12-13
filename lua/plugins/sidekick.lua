518c95ab04d82a5db0d52907831aad130e90e561
throwea <saeruig@saeruig.localdomain>
2025-12-13 00:46:35 -0600

    list projects where sidekick is disabled

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,3 +1,7 @@
+disable_projects = {
+  "/home/saeruig/Development/leetcode",
+  "/home/saeruig/Development/rust",
+}
 return {
   {
     "folke/sidekick.nvim",
@@ -6,21 +10,24 @@ return {
     },
     init = function()
       local cwd = vim.fn.getcwd()
-      if cwd ~= "/Users/elianahmar/Development/leetcode" then
-        vim.lsp.enable("copilot")
+      for _, path in ipairs(disable_projects) do
+        if cwd == path then
+          return
+        end
       end
+      vim.lsp.enable("copilot")
     end
   },
   {
     "zbirenbaum/copilot.lua",
     enabled = function()
-      return vim.fn.getcwd() ~= "/Users/elianahmar/Development/leetcode"
+      return vim.fn.getcwd() ~= "/home/saeruig/Development/leetcode"
     end
   },
   {
     "github/copilot.vim",
     enabled = function()
-      return vim.fn.getcwd() ~= "/Users/elianahmar/Development/leetcode"
+      return vim.fn.getcwd() ~= "/home/saeruig/Development/leetcode"
     end
   }
 }
